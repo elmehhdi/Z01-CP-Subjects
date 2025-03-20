4772e754f8af6eb5fb1e3c9a0a6be08b0c42a86e
el mehhdi <lmehdi0006@gmail.com>
2025-03-20 00:55:16 +0000

    cptest

--- 35percent/iscapitalized/iscapitalized.go
+++ 35percent/iscapitalized/iscapitalized.go
@@ -1,3 +1,43 @@
-p
-p
-p  
+package main
+
+import "fmt"
+
+func IsCapitalized(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	i := 0
+	n := len(s)
+
+	for i < n {
+		for i < n && s[i] == ' ' {
+			i++
+		}
+
+		if i >= n {
+			break
+		}
+
+		firstChar := s[i]
+
+		if firstChar >= 'a' && firstChar <= 'z' {
+			return false
+		}
+
+		for i < n && s[i] != ' ' {
+			i++
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(IsCapitalized("Hello! How are you?"))
+	fmt.Println(IsCapitalized("Hello How Are You"))
+	fmt.Println(IsCapitalized("Whats 4this 100K?"))
+	fmt.Println(IsCapitalized("Whatsthis4"))
+	fmt.Println(IsCapitalized("!!!!Whatsthis4"))
+	fmt.Println(IsCapitalized(""))
+}
